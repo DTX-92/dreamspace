1bceea097776604a21324cc977360d00124c67a3
Eugeny Panteleyev <epanteleyev@crocs.com>
2016-11-28 18:27:03 +0300

    28_11

--- public/src/app.component.js
+++ public/src/app.component.js
@@ -9,25 +9,66 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var components_1 = require("angular2-notifications/components");
+var core_2 = require('@angular/core');
+var components_1 = require("src/angular2-notifications/components");
 var AppComponent = (function () {
-    function AppComponent(_service) {
+    function AppComponent(_service, _ngZone) {
         this._service = _service;
+        this._ngZone = _ngZone;
+        this.progress = 0;
         this._service.success('asd', 'bsd');
         console.log(123);
     }
+    AppComponent.prototype.processWithinAngularZone = function () {
+        this.start = new Date().getTime();
+        this.label = 'inside';
+        this.progress = 0;
+        this._increaseProgress(function () { return console.log('Inside Done!'); });
+    };
+    // Loop outside of the Angular zone
+    // so the UI DOES NOT refresh after each setTimeout cycle
+    AppComponent.prototype.processOutsideOfAngularZone = function () {
+        var _this = this;
+        this.start = new Date().getTime();
+        this.label = 'outside';
+        this.progress = 0;
+        this._ngZone.runOutsideAngular(function () {
+            _this._increaseProgress(function () {
+                // reenter the Angular zone and display done
+                _this._ngZone.run(function () { console.log('Outside Done!'); });
+            });
+        });
+    };
+    AppComponent.prototype.sleep = function (ms) {
+        ms += new Date().getTime();
+        while (new Date() < ms) { }
+    };
+    AppComponent.prototype._increaseProgress = function (doneCallback) {
+        var _this = this;
+        this.progress += 1;
+        //console.log(`Current progress: ${this.progress}%`);
+        if (this.progress < 100) {
+            window.setTimeout(function () { return _this._increaseProgress(doneCallback); }, 10);
+        }
+        else {
+            doneCallback();
+            console.log(new Date().getTime() - this.start);
+        }
+    };
     AppComponent.prototype.onClickMe = function () {
         console.log(112);
-        this._service.success('asd');
+        //this._service.success('asd');
+        this._service.success('asd', 'bsd');
     };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<simple-notifications ></simple-notifications>\n<h1>Hello Angular!</h1>\n<button (click)=\"onClickMe()\">Button</button>"
+            template: "\n        <simple-notifications ></simple-notifications>\n        <h1>Hello Angular!</h1>\n        <button (click)=\"onClickMe()\">Button</button>\n        <p>Progress: {{progress}}%</p>\n        <p *ngIf=\"progress >= 100\">Done processing {{label}} of Angular zone!</p>\n        <button (click)=\"processWithinAngularZone()\">Process within Angular zone</button>\n        <button (click)=\"processOutsideOfAngularZone()\">Process outside of Angular zone</button>\n    "
         }), 
-        __metadata('design:paramtypes', [components_1.NotificationsService])
+        __metadata('design:paramtypes', [(typeof (_a = typeof components_1.NotificationsService !== 'undefined' && components_1.NotificationsService) === 'function' && _a) || Object, core_2.NgZone])
     ], AppComponent);
     return AppComponent;
+    var _a;
 }());
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- public/src/app.component.ts
+++ public/src/app.component.ts
@@ -1,23 +1,69 @@
 import {Component} from '@angular/core';
-import {NotificationsService} from "angular2-notifications/components";
+import { NgZone} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {NotificationsService} from "src/angular2-notifications/components";
 
 
 @Component({
     selector: 'my-app',
-    template: `<simple-notifications ></simple-notifications>
-<h1>Hello Angular!</h1>
-<button (click)="onClickMe()">Button</button>`
+    template: `
+        <simple-notifications ></simple-notifications>
+        <h1>Hello Angular!</h1>
+        <button (click)="onClickMe()">Button</button>
+        <p>Progress: {{progress}}%</p>
+        <p *ngIf="progress >= 100">Done processing {{label}} of Angular zone!</p>
+        <button (click)="processWithinAngularZone()">Process within Angular zone</button>
+        <button (click)="processOutsideOfAngularZone()">Process outside of Angular zone</button>
+    `
 })
 export class AppComponent {
+    progress: number = 0;
+    label: string;
+    start: number;
 
-    constructor(private _service:NotificationsService) {
+    constructor(private _service:NotificationsService, private _ngZone: NgZone) {
         this._service.success('asd', 'bsd');
         console.log(123);
     }
 
+    processWithinAngularZone() {
+        this.start = new Date().getTime();
+        this.label = 'inside';
+        this.progress = 0;
+        this._increaseProgress(() => console.log('Inside Done!'));
+    }
+    // Loop outside of the Angular zone
+    // so the UI DOES NOT refresh after each setTimeout cycle
+    processOutsideOfAngularZone() {
+        this.start = new Date().getTime();
+        this.label = 'outside';
+        this.progress = 0;
+        this._ngZone.runOutsideAngular(() => {
+            this._increaseProgress(() => {
+                // reenter the Angular zone and display done
+                this._ngZone.run(() => {console.log('Outside Done!') });
+            })});
+    }
+    sleep(ms) {
+
+        ms += new Date().getTime();
+        while (new Date() < ms){}
+    }
+    _increaseProgress(doneCallback: () => void) {
+        this.progress += 1;
+        //console.log(`Current progress: ${this.progress}%`);
+        if (this.progress < 100) {
+                window.setTimeout(() => this._increaseProgress(doneCallback), 10)
+        } else {
+            doneCallback();
+            console.log(new Date().getTime() - this.start);
+        }
+    }
+
     onClickMe() {
         console.log(112);
-        this._service.success('asd');
+        //this._service.success('asd');
+        this._service.success('asd', 'bsd');
     }
 
 }
