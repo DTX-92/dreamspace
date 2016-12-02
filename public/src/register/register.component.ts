8399ca71cb232886e859c665b2eda89864f0e327
Eugeny Panteleyev <epanteleyev@crocs.com>
2016-12-02 17:32:38 +0300

    Added register template and user model

---
+++ public/src/register/register.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by epanteleyev on 02.12.2016.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'register',
+    template: `
+        <input />`
+})
+
+export class RegisterComponent {
+
+}
---
+++ public/src/shared/models/user.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by epanteleyev on 02.12.2016.
+ */
